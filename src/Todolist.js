34d234f240eceb01911c1b172193c0478ee3d24b
Manikanta7013 <98421090+Manikanta7013@users.noreply.github.com>
2022-06-17 19:47:53 +0530

    Update Todolist.js

--- src/Todolist.js
+++ src/Todolist.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Todolist = ({text,del})=>{
   return(
     <div>
-      {text.map((item,index)=><div key={index}><h5 style={{color:'yellow'}}>{item} &nbsp;<i className="fa fa-trash-o" onClick={()=>del(index)}>delete</i></h5></div>)}
+      {text.map((item,index)=><div key={index}><h5 style={{color:'yellow'}}>{item} &nbsp;<button onClick={()=>del(index)}>delete</button></h5></div>)}
       </div>
   )
 }
